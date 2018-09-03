639ad7573419f2b2b63d17ad8a8b84a2b63c1273
Sérgio M. Basto <sergio@serjux.com>
2018-09-03 17:07:14 +0100

    New version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def bash_completion_dir():
 
 setup(
     name="rfpkg",
-    version="1.25.5",
+    version="1.25.6",
     author="Nicolas Chauvet",
     author_email="kwizart@gmail.com",
     description=("RPM Fusion plugin to rpkg to manage "

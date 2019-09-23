b56f3866b3e50093d398ee6715b7e12fc2fd36d4
Sérgio M. Basto <sergio@serjux.com>
2019-09-24 00:59:12 +0100

    Adjust MANIFEST.in and setup.py
    copy and update git-changelog from fedpkg
    requirements.txt and tests-requirements.txt coped from fedpkg

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python2
-from setuptools import setup
+#!/usr/bin/python
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
 try:
     from subprocess import getstatusoutput
-except:
+except ImportError:
     from commands import getstatusoutput
 
 
@@ -11,20 +16,64 @@ def bash_completion_dir():
         'pkg-config --variable=completionsdir bash-completion')
     return output if not sts and output else '/etc/bash_completion.d'
 
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+requirements = os.path.join(project_dir, 'requirements.txt')
+tests_requirements = os.path.join(project_dir, 'tests-requirements.txt')
+
+with open(requirements, 'r') as f:
+    install_requires = [line.strip() for line in f]
+
+with open(tests_requirements, 'r') as f:
+    tests_require = [line.strip() for line in f]
+
+ver = sys.version_info
+if ver[0] <= 2 and ver[1] < 7:
+    tests_require += [
+        'unittest2'
+    ]
+else:
+    install_requires += [
+        'distro',
+    ]
+
+
 setup(
     name="rfpkg",
-    version="1.25.6",
+    version="1.26.0",
     author="Nicolas Chauvet",
     author_email="kwizart@gmail.com",
     description=("RPM Fusion plugin to rpkg to manage "
                  "package sources in a git repository"),
     license="GPLv2+",
     url="https://github.com/rpmfusion-infra/rfpkg",
-    package_dir={'': 'src'},
-    packages=['rfpkg'],
-    scripts=['src/bin/rfpkg'],
-    data_files=[(bash_completion_dir(), ['src/rfpkg.bash']),
-                ('/etc/rpkg', ['src/rfpkg.conf']),
-                ('/usr/share/zsh/site-functions', ['src/_rfpkg']),
+    packages=find_packages(),
+    data_files=[(bash_completion_dir(), ['conf/bash-completion/rfpkg.bash']),
+                ('/etc/rpkg', ['conf/etc/rpkg/rfpkg.conf']),
+                ('/usr/share/zsh/site-functions', ['conf/zsh-completion/_rfpkg']),
                 ],
+
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+
+    entry_points={
+        'console_scripts': [
+            'rfpkg = rfpkg.__main__:main',
+        ],
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )

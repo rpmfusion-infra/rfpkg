e9d1593427384b2fed171c4e3772422844b7ab9e
Sérgio M. Basto <sergio@serjux.com>
2019-09-24 22:59:34 +0100

    Prepare a new version.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ else:
 
 setup(
     name="rfpkg",
-    version="1.26.0",
+    version="1.26.1",
     author="Nicolas Chauvet",
     author_email="kwizart@gmail.com",
     description=("RPM Fusion plugin to rpkg to manage "

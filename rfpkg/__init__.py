c70cc27b342427736bd67657160b339b1ddc1914
Sérgio M. Basto <sergio@serjux.com>
2026-01-30 17:47:51 +0000

    Add xorg-x11-drv-nvidia-580xx to multilib builds

--- rfpkg/__init__.py
+++ rfpkg/__init__.py
@@ -210,7 +210,7 @@ class Commands(pyrpkg.Commands):
             'mesa-freeworld', 'nvidia-vaapi-driver', 'intel-media-driver',
             'opencore-amr', 'rtmpdump', 'vo-amrwbenc', 'x264', 'x265', 'xvidcore', 'zsnes',
             'Cg', 'dega-sdl', 'gens', 'pcsx2', 'steam', 'xorg-x11-drv-nvidia',
-            'xorg-x11-drv-nvidia-470xx', 'xorg-x11-drv-nvidia-390xx',
+            'xorg-x11-drv-nvidia-580xx', 'xorg-x11-drv-nvidia-470xx', 'xorg-x11-drv-nvidia-390xx',
             'xorg-x11-drv-nvidia-340xx', 'unace'] and not self.branch_merge.startswith("el") and self.namespace in ['free', 'nonfree']:
             self._target += "-multilibs"
 
